764cec1a777f096fc714aa45cae64762ad0aba72
Brandon Untea <102970135+BrandonU27@users.noreply.github.com>
2022-09-06 16:32:20 -0400

    Added a var for date and time in the test class. Also made the test page that will show up after clicking a test.

--- app/src/main/java/com/example/testingflashy/TestClasses/Test.java
+++ app/src/main/java/com/example/testingflashy/TestClasses/Test.java
@@ -5,6 +5,13 @@ import java.util.List;
 
 public class Test {
     final List<Deck> deckList = new LinkedList<>();
+    final String date;
+    final String time;
+
+    Test(String _date, String _time){
+        date = _date;
+        time = _time;
+    }
 
     void AddDeck(Deck _deck){
         deckList.add(_deck);
